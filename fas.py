c62d44b9b392aecd2e70824170944b65337713f6
Mirivan <54078891+Mirivan@users.noreply.github.com>
2021-03-09 08:02:36 +0500

    Add files via upload

---
+++ fas.py
@@ -0,0 +1,72 @@
+# requires: lottie, pillow, wand
+import io, os, random
+from PIL import Image
+from wand.image import Image as IM
+""" module by mirivan """
+
+import logging
+
+from .. import loader, utils
+
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class FuckStickerMod(loader.Module)
+    strings = {"name": "(mirivan) FuckSticker"}
+
+    @loader.unrestricted
+    async def fascmd(self, message):
+        if message.is_reply:
+            reply = await message.get_reply_message()
+            if reply.photo:
+                a = message.pattern_match.group(1)
+                if a:
+                    if int(a) > 150:
+                        rescale_rate = 100
+                    else:
+                        rescale_rate = int(a)
+                else:
+                    rescale_rate = random.randint(35,100)
+                file = await message.client.download_media(reply_message.photo, bytes)
+                file, img = io.BytesIO(file), io.BytesIO()
+                img.name = 'img.png'
+                Image.open(file).save(img, 'PNG')
+                media = await distort(io.BytesIO(img.getvalue()), rescale_rate)
+                out, im = io.BytesIO(), Image.open(media)
+                if force_file:
+                    mime = 'png'
+                out.name = f'out.{mime}'
+                im.save(out, mime.upper())
+                out.seek(0)
+                await message.client.send_file(message.chat_id, out, reply_to=reply_message.id)
+        elif reply.file.name.endswith(".tgs"):
+            await message.edit("🐺 Обработка...")
+            await reply.download_media("tgs.tgs")
+            os.system("lottie_convert.py tgs.tgs json.json")
+            with open("json.json","r") as f: stick = f.read(); f.close()
+            for i in range(1, random.randint(6, 10)):
+                stick = random.choice([stick.replace(f'[{i}]', f'[{(i+i)*7}]'),
+                                                          stick.replace(f'[{i}]', f'[{(i+i)*6}]'),
+                                                          stick.replace(f'[{i}]', f'[{(i+i)*5}]'),
+                                                          stick.replace(f'[{i}]', f'[{(i+i)*4}]'),
+                                                          stick.replace(f'[{i}]', f'[{(i+i)*3}]')])
+                with open("json.json","w") as f: f.write(stick); f.close()
+            os.system("lottie_convert.py json.json tgs.tgs")
+            await reply.reply(file="tgs.tgs")
+            os.remove("tgs.tgs")
+            os.remove("json.json")
+            await message.delete()
+        else:
+            await message.edit('🐺 Прешелите любой тип стикера.')
+
+        async def distort(file, rescale_rate):
+            img = IM(file=file)
+            x, y = img.size[0], img.size[1]
+            popx = int(rescale_rate*(x//100))
+            popy = int(rescale_rate*(y//100))
+            img.liquid_rescale(popx, popy, delta_x=1, rigidity=0)
+            img.resize(x, y)
+            out = io.BytesIO()
+            out.name = f'output.png'
+            img.save(file=out)
+            return io.BytesIO(out.getvalue())
